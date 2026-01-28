ff4811367e91acecdc6d65efa2dc07724ef2a09d
Andrew Paterson <andrew.paterson9@gmail.com>
2026-01-28 16:09:18 +0200

    Remove node-sundries

--- run.js
+++ run.js
@@ -1,14 +1,30 @@
 const tagAndRelease = require('./index');
-const lib = require('node-sundries');
-const commitMessage = lib.getNamedArgVal('--commit-msg');
-const releaseType = lib.getNamedArgVal('--release-type');
+const commitMessage = getNamedArgVal('--commit-msg');
+const releaseType = getNamedArgVal('--release-type');
 
 if (!commitMessage) {
-  console.log('The --commit_msg argument is required.');
+  console.log('The --commit-msg argument is required.');
   return;
 }
 
 (async function () {
-  const res = await tagAndRelease.run({commitMessage: commitMessage, releaseType: releaseType});
+  const res = await tagAndRelease.run({ commitMessage: commitMessage, releaseType: releaseType });
   console.log(res);
-})()
+})();
+
+function getNamedArgVal(requested) {
+  const [, ...args] = process.argv;
+  let val;
+  args.forEach((arg) => {
+    if (arg.indexOf('=') < 0) {
+      return;
+    }
+
+    const argName = arg.split(/=(.*)/s)[0];
+
+    if (argName === requested) {
+      val = arg.split(/=(.*)/s)[1];
+    }
+  });
+  return val;
+}
